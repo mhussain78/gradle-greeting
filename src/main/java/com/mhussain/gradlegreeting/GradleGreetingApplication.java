943fa686a9a07041b95361b51252bf52f2351acc
Mohamad Hussain <inf.mhussain@googlemail.com>
2023-12-12 20:00:08 +0100

    initial version

---
+++ src/main/java/com/mhussain/gradlegreeting/GradleGreetingApplication.java
@@ -0,0 +1,13 @@
+package com.mhussain.gradlegreeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleGreetingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleGreetingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mhussain/gradlegreeting/GradleGreetingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhussain.gradlegreeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleGreetingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
